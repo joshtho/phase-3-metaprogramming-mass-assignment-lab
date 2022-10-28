7f007f38df8268395f5f91948ae565c36c9b3bdc
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-28 15:26:45 -0500

    commit tree c4e50cc79fd7a4b7410b7be326f5746a451f6fe8

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,12 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complextion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value| 
-      self.send("#{key}=", value)
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
     end
   end
 end
